35999c7763a3c02beecaf3c78e7cb7dda0ff0600
Musicalisk <musicalisk.travail@tuta.io>
2026-01-08 11:36:43 -0500

    added test file for python

---
+++ test.py
@@ -0,0 +1,12 @@
+a = 5
+b = 5
+c = 5 ** 3
+d = float(input('enter num: '))
+e = float(input('enter num: '))
+print(a + b / a ,'\n')
+print(31 // 3 ,'\n')
+# interger division ^
+print(c ,'\n')
+# cubing nums ^
+print('Quotient: ' + str(d/e))
+print('done')
