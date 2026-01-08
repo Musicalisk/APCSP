9649deb82f2036486ed95d7177b26bef590b808a
Musicalisk <musicalisk.travail@tuta.io>
2026-01-08 11:53:23 -0500

    Added more to test file

--- test.py
+++ test.py
@@ -3,10 +3,14 @@ b = 5
 c = 5 ** 3
 d = float(input('enter num: '))
 e = float(input('enter num: '))
+num1 = float(input('Enter num: '))
+num2 = float(input('Enter num: '))
 print(a + b / a ,'\n')
 print(31 // 3 ,'\n')
 # interger division ^
 print(c ,'\n')
 # cubing nums ^
-print('Quotient: ' + str(d/e))
+print('Quotient: ' + str(d/e) + '\n')
+print(num1 + num2,'\n')
+print(5%2, 2%7)
 print('done')
