84abc926151d9dfabb17c85fbb9766fd5c4ce8ef
Musicalisk <musicalisk.travail@tuta.io>
2026-01-08 22:06:34 -0500

    Fixed error by addidng absolute value before applying square root function on inputted value

--- 2.4-q1.py
+++ 2.4-q1.py
@@ -2,5 +2,5 @@
 # Use the absolute value function to make sure that if the user enters a negative number, the program does not crash.
 # Lastly, write a print statement that states "The final outcome is " followed by the square root and remember to change the final outcome to a string.
 import math
-num1=int(input('Enter a number: '))
-print('The final outcome is:',math.sqrt(num1))
+num1=math.sqrt(math.fabs(float(input('Enter a number: '))))
+print('The final outcome is:',num1)
