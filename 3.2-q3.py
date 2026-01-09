ae9f881625126e9bf2a98a39cca3b0806c13ed87
Musicalisk <musicalisk.travail@tuta.io>
2026-01-09 11:33:21 -0500

    Added assignment 3.2 Question 3

---
+++ 3.2-q3.py
@@ -0,0 +1,5 @@
+# Test if a password entered is correct.
+# The secret phrase is “Amedeo Avogadro” (without the quotes).
+key=input('Enter the password: ')
+if(key=='Amedeo Avogadro'):print('Correct!')
+else:print('Not Correct')
