d5b1fdc842ec0bd231ecfa00235060f19438e6a6
Musicalisk <musicalisk.travail@tuta.io>
2026-01-09 10:54:03 -0500

    Added assignment 2.7 Question 2

---
+++ 2.7-q2.py
@@ -0,0 +1,5 @@
+# Write a program to calculate the farthest in each direction that the turtle was located throughout its travels.
+# Add four print statements to the lines of code above that output the following, where the number signs are replaced with the correct values from the correct list:
+lat=[15.18,15.11,15.12,15.12,15.08,14.95,14.87,14.81,14.81,14.75,14.73,14.68,14.55]
+lon=[-62.942,-62.807,-62.622,-62.499,-62.438,-62.372,-62.352,-62.318,-62.321,-62.201,-62.150,-62.154,-61.915]
+print('Farthest north is',max(lat),'\nFarthest west is',min(lon),'\nFarthest south is',min(lat),'\nFarthest east is',max(lon))
