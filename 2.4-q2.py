4e334e18434938dfbf21ba5ea53c6110d13a96f1
Musicalisk <musicalisk.travail@tuta.io>
2026-01-08 23:46:10 -0500

    Changes Import fashion on 2.4 lesson files

--- 2.4-q2.py
+++ 2.4-q2.py
@@ -1,7 +1,7 @@
 # Write a program that accepts a number as input, and prints just the decimal portion.
 # Your program should also work if a negative number is inputted by the user.
 # Lastly, write a print statement that states “The final outcome is: ”, followed by the decimal portion, and remember to change the final outcome to a string.
-import math
-num1=math.fabs(float(input('Enter a number: ')))
+from math import *
+num1=fabs(float(input('Enter a number: ')))
 num2=int(num1)
 print('The final outcome is:',num1-num2)
