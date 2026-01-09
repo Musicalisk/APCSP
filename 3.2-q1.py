b0f8bfe46b354c81d852512162bb8bb786e4e764
Musicalisk <musicalisk.travail@tuta.io>
2026-01-09 11:22:55 -0500

    Added assignment 3.2 Question 1

---
+++ 3.2-q1.py
@@ -0,0 +1,4 @@
+# Write a program to enter a decimal number and test if it is less than 26.4.
+# If the number entered is less than 26.4, the program needs to output the phrase Less than 26.4.
+num1=float(input('Enter a number: '))
+if(num1<26.4):print('Less than 26.4')
