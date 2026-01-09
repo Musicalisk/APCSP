6ad5f999763320e66702b393248d2a210f2e6638
Musicalisk <musicalisk.travail@tuta.io>
2026-01-08 20:45:31 -0500

    Added aditional lines to test.py and further remodelled code for.

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-a = 5
-b = 5
-c = 5 ** 3
-d = float(input('enter num: '))
-e = float(input('enter num: '))
-num1 = float(input('Enter num: '))
-num2 = float(input('Enter num: '))
+print('Run Started!\n')
+a=5
+b=5
+c= 5**3
+d=float(input('Enter num: '))
+e=float(input('Enter num: '))
+num1=float(input('Enter num: '))
+num2=float(input('Enter num: '))
 print('1',a+b/a)
 print('2',31//3)
 # interger division ^
