8e5cc26997116530eea86c96fd223d48722cf5a9
Musicalisk <musicalisk.travail@tuta.io>
2026-01-08 21:47:49 -0500

    Added assignment 2.4 Question 1

---
+++ 2.4-q1.py
@@ -0,0 +1,6 @@
+# Write the code to input a number and calculate the square root.
+# Use the absolute value function to make sure that if the user enters a negative number, the program does not crash.
+# Lastly, write a print statement that states "The final outcome is " followed by the square root and remember to change the final outcome to a string.
+import math
+num1=int(input('Enter a number: '))
+print('The final outcome is:',math.sqrt(num1))
