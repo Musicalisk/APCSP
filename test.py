162ac1a19c50a442df1c9b6f75980a018bd607ce
Musicalisk <musicalisk.travail@tuta.io>
2026-01-09 11:58:39 -0500

    Remodelled structure of file

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 print('Run Started!\n')
 a=5
 b=5
-c= 5**3
+c=5**3
 d=float(input('Enter num: '))
 e=float(input('Enter num: '))
 num1=float(input('Enter num: '))
@@ -11,10 +11,10 @@ print('2',31//3)
 # interger division ^
 print('3',c)
 # cubing nums ^
-print('4 Quotient: '+str(d/e))
+print('4','Quotient:',str(d/e))
 print('5',num1+num2)
 print('6',5%2,2%7)
-print('7',min("cat", "dog", "iguana", "anteater", "fish", "aardvark"))
-print('8',max("cat", "dog", "iguana", "anteater", "fish", "aardvark"))
+print('7',min("cat","dog","iguana","anteater","fish","aardvark"))
+print('8',max("cat","dog","iguana","anteater","fish","aardvark"))
 # min and max for Data sets ^
 print('\nRun Complete!')
