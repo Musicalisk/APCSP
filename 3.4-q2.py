11f8645e31ea11ca9adabd844f12dfbe6e6da654
Musicalisk <musicalisk.travail@tuta.io>
2026-01-10 03:42:24 -0500

    Added assignment 3.4 Question 2

---
+++ 3.4-q2.py
@@ -0,0 +1,9 @@
+# Write a program to convert a fraction to a decimal. 
+# Have your program ask for the numerator first, then the denominator. 
+# Make sure to check if the denominator is zero. 
+# If it is, print out "Cannot divide by zero."
+# Hint: Since this lesson uses if-else statements, remember to use at least one if-else statement in each of your answers to receive full credit.
+top=int(input('Enter the numerator: '))
+bottom=int(input('Enter the denominator: '))
+if bottom==0:print('Cannot be divided by zero.')
+else:print('Decimal:',top/bottom)
