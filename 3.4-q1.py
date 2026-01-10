0b3f7a91d533385d622e26b95102015e02b496ac
Musicalisk <musicalisk.travail@tuta.io>
2026-01-10 03:34:47 -0500

    Added assignment 3.4 Question 1

---
+++ 3.4-q1.py
@@ -0,0 +1,6 @@
+# Input a word. 
+# If it is "orange," print "Correct." Otherwise, print "Nope". 
+# What happens if you type in "ORANGE" or "OrANGE?" Does the capitalizing make a difference?
+color=input('what color? ')
+if color=='orange':print('Correct') 
+else:print('Nope')
