1fca8cd2c66c99c26058a3dcb9bc59ca7fd94f25
Musicalisk <musicalisk.travail@tuta.io>
2026-01-11 18:09:04 -0500

    Added changes to files

--- 4.3-q2.py
+++ 4.3-q2.py
@@ -1,2 +1,6 @@
 # Write a program that uses a while loop to calculate and print the multiples of 4 from 4 to 24. 
 # Your program should print each number on a separate line.
+num=0
+while num!=24:
+    num+=4
+    print(num)
