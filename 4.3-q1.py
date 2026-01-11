89d3a4c8e625d720ad80d3c849f5251aef88d20d
Musicalisk <musicalisk.travail@tuta.io>
2026-01-11 18:08:45 -0500

    Added changes to files

--- 4.3-q1.py
+++ 4.3-q1.py
@@ -2,3 +2,9 @@
 # When you turned 15, she made you 15 cookies! 
 # This year, she, unfortunately, cannot see you on your birthday, so instead, she wants to send you virtual cookies!
 # Create a program that prompts the user for how old they are turning, and then, using a loop, outputs a virtual cookie for every year old they are.
+from math import fabs
+cookie='(::)'
+counter=fabs(int(input('How old are you? ')))
+while counter!=0:
+    print(cookie)
+    counter-=1
