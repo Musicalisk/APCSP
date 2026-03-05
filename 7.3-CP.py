192157437f01bcca7679cf09e03e633117840e14
Musicalisk <musicalisk.travail@tuta.io>
2026-03-05 11:24:23 -0500

    Addded Assignment 7.3 Coding Practice

---
+++ 7.3-CP.py
@@ -0,0 +1,7 @@
+def print_product(a,b,c):
+    out=a*b*c
+    print(out)
+ha=int(input('Enter the 1st number: '))
+hb=int(input('Enter the 2nd number: '))
+hc=int(input('Enter the 3rd number: '))
+print_product(ha,hb,hc)
